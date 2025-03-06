b29ebdd4402a8396f3c66496d8e06cafd360aed5
MachineLearning <rzadanowsky@albany.edu>
2025-03-05 22:53:04 -0500

    For 404

--- src/ALU.java
+++ src/ALU.java
@@ -1,11 +1,31 @@
+// The ALU class holds and processes sets of instructions.
 public class ALU {
+
+    // The instruction set.
     public Word16 instruction = new Word16();
+
+    // The 1st operator.
     public Word32 op1 = new Word32();
+
+    //The 2nd operator.
     public Word32 op2 = new Word32();
+
+    // To hold the resulting value after the specified instruction.
     public Word32 result = new Word32();
+
+    // Used for the compare instruction, sets the less flag.
     public Bit less = new Bit(false);
+
+    // Used for the compare instruction, sets the equal flag.
     public Bit equal = new Bit(false);
 
+
+    /**
+     * This method computes for the opcode which is then utilized to match to a specific instruction.
+     * When a match is found that corresponding instruction is executed accordingly. The instruction
+     * descriptions include add, and, multiply, left shift, subtract, or, right shift, and compare.
+     *
+     */
     public void doInstruction() {
         result = new Word32();
         int startingOpCode = 0;
@@ -52,13 +72,22 @@ public class ALU {
                 break;
         }
     }
-        public int conversionSmallRange(Word32 subset_op2) {
-            int amountToShift = 0;
-            for (int i = 27; i < 32; i++) {
-                if(subset_op2.word32[i].getValue() == Bit.boolValues.TRUE)
-                    amountToShift += calculate2Raised(31 - i);
-            }
-            return amountToShift;
+
+    /**
+     * This method takes in a Word32 opcode and then proceeds to do a bit to integer conversion
+     * of its last 5 bits only when those bit values hold a value of TRUE. Then, the newly converted
+     * value is returned as the amount to be shifted either via left or right shift.
+     *
+     * @param subset_op2 The Word32 instance.
+     * @return The amount to be shifted.
+     */
+    public int conversionSmallRange(Word32 subset_op2) {
+        int amountToShift = 0;
+        for (int i = 27; i < 32; i++) {
+            if(subset_op2.word32[i].getValue() == Bit.boolValues.TRUE)
+                amountToShift += calculate2Raised(31 - i);
+        }
+        return amountToShift;
     }
     /**
      * This method takes in a number which determines the number of times the base is multiplied
@@ -74,6 +103,18 @@ public class ALU {
         }
         return accumulator;
     }
+
+    /**
+     * This method takes in two Word32 instances and compares their values bit by bit. If
+     * the first Word32 instance is bigger than the second Word32 instance then a result of 1
+     * is returned. If the first Word32 instance is smaller than the second Word32 instance then a
+     * result of -1 is returned. Otherwise, both Word32 instances are equivalent and a value of
+     * 0 is returned.
+     *
+     * @param sample1 The first Word32 instance.
+     * @param sample2 The second Word32 instance.
+     * @return The comparison status.
+     */
     public int compare(Word32 sample1, Word32 sample2) {
         //Check for signed bits.
         if(sample1.word32[0].getValue() != sample2.word32[0].getValue()) {
@@ -110,6 +151,15 @@ public class ALU {
         }
         return 0;
     }
+
+    /**
+     * This method proceeds to compute the opcode by processing 5 bits from the given instruction.
+     * Then it's multiplied by 2 which the value then gets added by 1 if the corresponding
+     * bit holds the value TRUE, and finally it returns the resulting value.
+     *
+     * @param startingOpCode The starting value for the opcode.
+     * @return The computed opcode.
+     */
     public int returnOpcode(int startingOpCode) {
         for (int i = 0; i < 5; i++) {
             startingOpCode *= 2;
--- src/Memory.java
+++ src/Memory.java
@@ -1,9 +1,22 @@
+// The Memory Class is where data can be loaded to and where later this same data can be accessed (read) from
+// or modified (write) to.
 public class Memory {
-    public Word32 address= new Word32();
+
+    // The address or location of the data in memory.
+    public Word32 address = new Word32();
+
+    // The actual data stored in memory to either be read from or written to.
     public Word32 value = new Word32();
 
+    // The main memory.
     private final Word32[] dram= new Word32[1000];
 
+    /**
+     * This method converts a 32-bit address into an integer which signifies a
+     * valid memory location within the range of 0-999 and returns the newly converted value.
+     *
+     * @return The memory location.
+     */
     public int addressAsInt() {
         int accumulator  = 0;
         for(int i  = 0; i < 32; i++) {
@@ -15,22 +28,47 @@ public class Memory {
         return accumulator;
     }
 
+    /**
+     * This constructor initializes the main memory, where each slot it set to hold
+     * a default Word32 object or longword.
+     *
+     */
     public Memory() {
         for(int i  = 0; i < dram.length; i++) {
             dram[i] = new Word32();
         }
     }
 
+    /**
+     * This method retrieves the data from the memory using the specified address
+     * and copies it over to be accessed.
+     *
+     */
     public void read() {
         int index = addressAsInt();
         dram[index].copy(value);
     }
 
+    /**
+     * This method writes/modifies data in the memory using the specified address.
+     *
+     */
     public void write() {
         int index = addressAsInt();
         value.copy(dram[index]);
     }
 
+    /**
+     * This method takes in an array of Strings with each index holding a 32 character length
+     * String with each character being either 't' or 'f'. Then as we iterate through the
+     * array of Strings, we iterate through each of the characters of the 32 length String contained
+     * within. If the character is 't' then we assign the corresponding bit value for a Word32 instance
+     * to hold TRUE and otherwise false. Once the 32 length String has been processed, the initialized
+     * Word32 (data) is copied over to the memory. This keeps executing until all the data
+     * has been processed.
+     *
+     * @param data To load to memory.
+     */
     public void load(String[] data) {
         for(int i = 0; i < data.length; i++) {
             Word32 temp = new Word32();
