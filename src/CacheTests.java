802ccd9951ee347fa8650cf870cc20a73f9d6fd9
MachineLearning <rzadanowsky@albany.edu>
2025-04-23 12:51:26 -0400

    For 404

--- src/CacheTests.java
+++ src/CacheTests.java
@@ -2,38 +2,45 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// The CacheTests class contains my Testing Programs for the Cache. These test programs include
+// summing 20 integers in an array, creating a 20-item linked list and summing them, and finally
+// summing 20 integers in an array backwards.
 public class CacheTests {
 
-    // Case 1:
+    /**
+     * This method to test the performance using a test program of summing 20 integers in an array.
+     * Measures clock cycle count, checks elements of the array, and also the total sum of the array.
+     * Case 1: Summing 20 integers in an array.
+     */
     @Test
     public void SumIntegersTest() {
         String[] sumArrayInt = {
-                "copy 10 r6",
+                "copy 10 r6",       // Line 1
                 "multiply 10 r6",
-                "multiply 4 r6",
-                "copy r6 r0", // Get address space 400 to r0
-                "copy r6 r0",
-                "copy 15 r1", // Fill in value 1
-                "copy 10 r2",
-                "multiply 2 r2", // Length 20
-                "copy 1 r5",
-                "store r1 r0",
-                "add r5 r0",
-                "subtract 1 r2",
-                "compare 0 r2",
-                "bne -2",
-                "copy 10 r0",
+                "multiply 4 r6",    // Line 2
+                "copy r6 r0",       // Give address space 400 to r0
+                "copy r6 r0",       // Line 3 - Dummy
+                "copy 15 r1",       // Data for each element in the array
+                "copy 10 r2",       // Line 4
+                "multiply 2 r2",    // Length 20 for the array
+                "copy 1 r5",        // Line 5
+                "store r1 r0",      // Stores value 15 in memory at address 400
+                "add r5 r0",        // Line 6 - increment pointer by 1 to the next memory address
+                "subtract 1 r2",    // Decrement length counter by 1
+                "compare 0 r2",     // Line 7 - checks if the length counter of the array is 0
+                "bne -2",           // If the array is still not fully processed, loop back to line 5
+                "copy 10 r0",       // Line 8
                 "multiply 10 r0",
-                "multiply 4 r0", // Get address space 400 to r0
+                "multiply 4 r0",     // Line 9 - Give address space 400 to r0
                 "copy 10 r2",
-                "multiply 2 r2", // Length 20
-                "copy 0 r3", // Accumulator
-                "load r0 r4", //Sum the array until length is decremented from 20 to 0.
-                "add r4 r3",
-                "add r5 r0",
-                "subtract 1 r2",
-                "compare 0 r2",
-                "bne -2",
+                "multiply 2 r2",     // Line 10 - Length 20
+                "copy 0 r3",         // Accumulator initialized to 0
+                "load r0 r4",        // Line 11 - load the value on memory address 400 into r4
+                "add r4 r3",         // Add value to the accumulator
+                "add r5 r0",         // Line 12 - increment pointer by 1 to the next memory address
+                "subtract 1 r2",     // Decrement length counter by 1
+                "compare 0 r2",      // Line 13 - checks if the length counter of the array is 0
+                "bne -2",            // If the array is still not fully processed, loop back to line 11
                 "halt"
         };
         System.out.println("Case 1: Array of size 20 and sum: ");
@@ -100,40 +107,44 @@ public class CacheTests {
         assertEquals("402:f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,t,f,t,f,", processor2.output.get(2));
         assertEquals("403:f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,t,t,f,f,t,f,t,f,f,", processor2.output.get(3));
     }
-    // Case 3:
+    /**
+     * This method to test the performance using a test program of summing 20 integers in an array backwards.
+     * Measures clock cycle count, checks elements of the array, and also the total sum of the array.
+     *  Case 3: Summing 20 integers in an array backwards (end to beginning).
+     */
     @Test
     public void SumIntegersBackwardsTest() {
         String[] sumArrayIntBackwards = {
-                "copy 10 r0",
+                "copy 10 r0",       // Line 1
                 "multiply 10 r0",
-                "multiply 4 r0", // Get address space 400 to r0
-                "copy 6 r1", // Fill in value 1
-                "copy 10 r2",
-                "multiply 2 r2", // Length 20
-                "copy 1 r5",
-                "store r1 r0",
-                "add r5 r0",
-                "subtract 1 r2",
-                "compare 0 r2",
-                "bne -2",
-                "copy 10 r6",
+                "multiply 4 r0",    // Line 2 - Give address space 400 to r0
+                "copy 6 r1",        // Data for each element in the array
+                "copy 10 r2",       // Line 3
+                "multiply 2 r2",    // Length 20 for the array
+                "copy 1 r5",        // Line 4
+                "store r1 r0",      // Store value 6 in the memory address 400
+                "add r5 r0",        // Line 5 - increment the pointer by 1 to the next memory address
+                "subtract 1 r2",    // Decrement the length counter by 1
+                "compare 0 r2",     // Line 6 - checks if the length counter of the array is 0
+                "bne -2",           // If the array is still not fully processed, loop back to line 4
+                "copy 10 r6",       // Line 7
                 "multiply 10 r6",
+                "multiply 2 r6",    // Line 8
                 "multiply 2 r6",
-                "multiply 2 r6",
-                "add 15 r6",
+                "add 15 r6",        // Line 9 - Get address space 419
                 "add 4 r6",
-                "copy 0 r0", // Dummy
+                "copy 0 r0",        // Line 10
                 "copy 0 r0",
-                "add r6 r0", // Get address space 419 to r0
+                "add r6 r0",        // Line 11 - Give address space 419 to r0
                 "copy 10 r2",
-                "multiply 2 r2", // Length 20 {length}
-                "copy 0 r3", // Accumulator
-                "load r0 r4", //Sum the array until length is decremented from 20 to 0.
-                "add r4 r3",
-                "subtract r5 r0",
-                "subtract 1 r2",
-                "compare 0 r2",
-                "bne -2",
+                "multiply 2 r2",    // Line 12 - Length 20 for the array
+                "copy 0 r3",        // Initialize the accumulator
+                "load r0 r4",       // Line 13 - load the value in memory address 419 to r4
+                "add r4 r3",        // Add the value to the accumulator
+                "subtract r5 r0",   // Line 14 - decrement the pointer by 1 to the next memory address
+                "subtract 1 r2",    // Decrement the length counter by 1
+                "compare 0 r2",     // Line 15 - checks if the length counter of the array is 0
+                "bne -2",           // If the array is still not fully processed, loop back to line 13
                 "halt"
         };
         System.out.println("Case 3: Array of size 20 and sum backwards: ");
--- src/InstructionCache.java
+++ src/InstructionCache.java
@@ -1,8 +1,25 @@
+
+// The Instruction Cache is responsible for reading instructions from L2 Cache and then
+// returning those instructions to the processor every time the processor fetches an instruction.
 public class InstructionCache {
+
+    // To count the clock cycle in the Instruction Cache.
     public int InstructionCacheClockCycle;
+
+    // The L2 Cache instance to read instructions from the main memory to the Instruction Cache.
     public L2Cache l2Cache;
+
+    // The array of slots in the Instruction Cache to store 8-word instructions and an
+    // extra slot to store the address represented by the first of these 8-word instructions.
     public Word32[] slots;
 
+    /**
+     * This constructor takes in an L2 Cache and sets it to the L2 Cache instance field. It
+     * also sets the array of slots to a size of 9 and initializes each slot to a default Word32
+     * instance.
+     *
+     * @param l2Cache The L2 Cache instance.
+     */
     public InstructionCache(L2Cache l2Cache) {
         this.l2Cache = l2Cache;
         slots = new Word32[9] ;
